5011775a054476a1182710822cdc1d702c4cefd1
DEEPAKNANI123 <deepaknani1436@gmail.com>
2026-02-12 18:09:38 +0530

    Added psycopg2 driver

--- database.py
+++ database.py
@@ -3,11 +3,15 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+# Load .env file
+load_dotenv(dotenv_path=".env")
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 print("DATABASE_URL =", DATABASE_URL)
+
+if DATABASE_URL is None:
+    raise Exception("❌ DATABASE_URL not found. Check your .env file location.")
 
 engine = create_engine(DATABASE_URL)
 
