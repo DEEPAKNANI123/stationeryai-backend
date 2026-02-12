569f280831eed81f40cd8b7cbb03db41620ee4a1
DEEPAKNANI123 <deepaknani1436@gmail.com>
2026-02-12 16:09:08 +0530

    Fix render python version and requirements

--- database.py
+++ database.py
@@ -1,12 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-import os
+from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+print("DATABASE_URL =", DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 
